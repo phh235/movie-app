96dd657707773df2083a67bb2571686c5aa7a011
Andrew Hoang <hoangaatrox23504@gmail.com>
2025-02-18 23:19:40 +0700

    add useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,11 +19,16 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [deboundSearchTerm, setDeboundSearchTerm] = useState('');
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDeboundSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -44,8 +50,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
+    fetchMovies(deboundSearchTerm);
+  }, [deboundSearchTerm])
 
   return (
     <main>
